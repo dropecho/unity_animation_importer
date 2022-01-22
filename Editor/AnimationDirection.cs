1ecf47e642d361d953a654c30890faf992bd9474
Ben Van Treese <vantreeseba@gmail.com>
2022-01-22 12:26:31 -0600

    refactor: Name anim direction menu items in a clearer way.

--- Editor/AnimationDirection.cs
+++ Editor/AnimationDirection.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 namespace Dropecho {
   public class AnimationDirectionContext {
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/0 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/0 Deg (Forward)", false, 10)]
     static void ChangeAnimationDirectionTo0(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, Vector3.forward);
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/90 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/90 Deg (Right)", false, 10)]
     static void ChangeAnimationDirectionTo90(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, Vector3.right);
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/-90 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/-90 Deg (Left)", false, 10)]
     static void ChangeAnimationDirectionToNeg90(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, Vector3.left);
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/45 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/45 Deg (Forward + Right)", false, 10)]
     static void ChangeAnimationDirectionTo45(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, (Vector3.forward + Vector3.right).normalized);
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/-45 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/-45 Deg (Forward + Left)", false, 10)]
     static void ChangeAnimationDirectionToNeg45(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, (Vector3.forward + Vector3.left).normalized);
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/135 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/135 Deg (Backwards + Right)", false, 10)]
     static void ChangeAnimationDirectionTo135(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, (Vector3.back + Vector3.right).normalized);
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/-135 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/-135 Deg (Backwards + Left)", false, 10)]
     static void ChangeAnimationDirectionToNeg135(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, (Vector3.back + Vector3.left).normalized);
-    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/180 Deg", false, 10)]
+    [UnityEditor.MenuItem("CONTEXT/AnimationClip/Direction/180 Deg (Backwards)", false, 10)]
     static void ChangeAnimationDirectionTo180(UnityEditor.MenuCommand menuCommand) => ChangeAnimationDirection(menuCommand, (Vector3.back).normalized);
 
     static void ChangeAnimationDirection(UnityEditor.MenuCommand menuCommand, Vector3 changeTo) {
